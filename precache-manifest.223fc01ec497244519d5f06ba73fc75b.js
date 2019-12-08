97e4cd7223e65b6cbf3316a455b64cda9b07f4fe
Doğukan Arslan <dogukan2arslan@hotmail.com>
2019-12-09 01:26:50 +0300

    Updates

---
+++ precache-manifest.223fc01ec497244519d5f06ba73fc75b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b4e54dac2e1c7e93bbaa1b943a791c",
+    "url": "/pet-application-react/index.html"
+  },
+  {
+    "revision": "b2088a35cd2fbced42c5",
+    "url": "/pet-application-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "91425536aa7e2dfa9680",
+    "url": "/pet-application-react/static/css/main.7bf197c9.chunk.css"
+  },
+  {
+    "revision": "b2088a35cd2fbced42c5",
+    "url": "/pet-application-react/static/js/2.98d1e7ea.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/pet-application-react/static/js/2.98d1e7ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91425536aa7e2dfa9680",
+    "url": "/pet-application-react/static/js/main.12c94b1c.chunk.js"
+  },
+  {
+    "revision": "f934e90a549da226babb",
+    "url": "/pet-application-react/static/js/runtime-main.a2e961bd.js"
+  }
+]);
